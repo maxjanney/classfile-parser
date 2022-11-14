f0b2674c3968efc96d6dacf1ff7b12b052d82647
maxjanney <majanney@me.com>
2022-11-14 11:57:53 -0500

    finish attribute

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+// todo: split this up into modules
+
 pub type U1 = u8;
 pub type U2 = u16;
 pub type U4 = u32;
@@ -88,8 +90,8 @@ pub enum ConstantPoolTag {
 }
 
 impl From<u8> for ConstantPoolTag {
-    fn from(byte: u8) -> Self {
-        match byte {
+    fn from(b: u8) -> Self {
+        match b {
             9 => Self::Fieldref,
             7 => Self::Class,
             10 => Self::Methodref,
@@ -117,7 +119,7 @@ pub struct FieldInfo {
     pub attributes: Vec<Attribute>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum AttributeType {
     ConstantValue,
     Code,
@@ -141,7 +143,7 @@ pub enum AttributeType {
     BootstrapMethods,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Attribute {
     ConstantValue {
         constant_value_index: U2,
@@ -156,10 +158,58 @@ pub enum Attribute {
     StackMapTable {
         entries: Vec<StackMapFrame>,
     },
-    // TODO: Rest of attributes
+    Exceptions {
+        number_of_exceptions: U2,
+        exception_index_table: Vec<U2>,
+    },
+    InnerClasses {
+        classes: Vec<InnerClass>,
+    },
+    EnclosingMethod {
+        class_index: U2,
+        method_index: U2,
+    },
+    Synthetic,
+    Signature {
+        signature_index: U2,
+    },
+    SourceFile {
+        source_file_index: U2,
+    },
+    SourceDebugExtension {
+        debug_extension: Vec<U1>,
+    },
+    LineNumberTable {
+        line_number_table: Vec<LineNumber>,
+    },
+    LocalVariableTable {
+        local_variable_table: Vec<LocalVariable>,
+    },
+    LocalVariableTypeTable {
+        local_variable_type_table: Vec<LocalVariable>,
+    },
+    Deprecated,
+    RuntimeVisibleAnnotations {
+        annotations: Vec<Annotation>,
+    },
+    RuntimeInvisibleAnnotations {
+        annotations: Vec<Annotation>,
+    },
+    RuntimeVisibleParameterAnnotations {
+        annotations: Vec<Annotation>,
+    },
+    RuntimeInvisibleParameterAnnotations {
+        annotations: Vec<Annotation>,
+    },
+    AnnotationDefault {
+        default_value: ElementValue,
+    },
+    BootstrapMethods {
+        bootstrap_methods: Vec<BootstrapMethod>,
+    },
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct ExceptionHandler {
     pub start_pc: U2,
     pub end_pc: U2,
@@ -167,7 +217,7 @@ pub struct ExceptionHandler {
     pub catch_type: U2,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum StackMapFrame {
     Same {
         tag: U1,
@@ -204,7 +254,7 @@ pub enum StackMapFrame {
     },
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum VerificationTypeInfo {
     TopVariable,
     IntegerVariable,
@@ -215,4 +265,104 @@ pub enum VerificationTypeInfo {
     UninitializedThisVariable,
     ObjectVariable { cpool_index: U2 },
     UninitializedVariable { offset: U2 },
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct InnerClass {
+    pub inner_class_info: U2,
+    pub outer_class_info: U2,
+    pub inner_name_index: U2,
+    pub inner_class_access_flags: U2,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct LineNumber {
+    pub start_pc: U2,
+    pub line_number: U2,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct LocalVariable {
+    pub start_pc: U2,
+    pub length: U2,
+    pub name_index: U2,
+    pub descriptor_index: U2,
+    pub index: U2,
+}
+
+#[derive(Debug, Clone)]
+pub struct Annotation {
+    pub type_index: U2,
+    pub element_value_pairs: Vec<ElementValuePair>,
+}
+
+#[derive(Debug, Clone)]
+pub struct ElementValuePair {
+    pub element_name_index: U2,
+    pub value: ElementValue,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum ElementValueTag {
+    Byte,
+    Char,
+    Double,
+    Float,
+    Int,
+    Long,
+    Short,
+    Boolean,
+    String,
+    EnumConstant,
+    Class,
+    Annotation,
+    Array,
+    Unknown,
+}
+
+impl From<u8> for ElementValueTag {
+    fn from(b: u8) -> Self {
+        match b {
+            b'B' => Self::Byte,
+            b'C' => Self::Char,
+            b'D' => Self::Double,
+            b'F' => Self::Float,
+            b'I' => Self::Int,
+            b'J' => Self::Long,
+            b'S' => Self::Short,
+            b'Z' => Self::Boolean,
+            b's' => Self::String,
+            b'e' => Self::EnumConstant,
+            b'c' => Self::Class,
+            b'@' => Self::Annotation,
+            b'[' => Self::Array,
+            _ => Self::Unknown,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum ElementValue {
+    ConstValueIndex {
+        index: U2,
+    },
+    EnumConstValue {
+        type_name_index: U2,
+        const_name_index: U2,
+    },
+    ClassInfoIndex {
+        index: U2,
+    },
+    AnnotationValue {
+        annotation_value: Annotation,
+    },
+    ArrayValue {
+        values: Vec<ElementValue>,
+    },
+}
+
+#[derive(Debug, Clone)]
+pub struct BootstrapMethod {
+    pub bootstrap_method_ref: U2,
+    pub bootstrap_arguments: Vec<U2>,
 }
